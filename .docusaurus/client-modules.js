5db3ab173b48ee8314f0e6cf7e0770dcaccdc20e
hiroshiprover <12312612@mail.sustech.edu.cn>
2026-05-16 23:12:28 +0800

    Deploy website - based on 557e435cf5884a3ef27a10418ccaef7b9803e5bd

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/hiroshiprover/Documents/my-site/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/hiroshiprover/Documents/my-site/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/hiroshiprover/Documents/my-site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/hiroshiprover/Documents/my-site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/hiroshiprover/Documents/my-site/src/css/custom.css"),
-];
